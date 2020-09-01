edf6a0933764b581177642314e775ef60ffb5968
lagofelipe <lagofelipe@gmail.com>
2020-09-01 14:18:12 -0500

    Done.

---
+++ lib/artist.rb
@@ -0,0 +1,31 @@
+class Artist
+    attr_accessor :name, :song
+    
+    
+    def initialize(name)
+        @name = name
+    end
+
+    def songs
+    Song.all.select do |song|
+        song.artist == self
+    end
+end
+
+def add_song(song) 
+song.artist = self
+end
+
+def add_song_by_name(song_name)
+    musician = Song.new(song_name)
+    add_song(musician)
+end
+
+def self.song_count
+    Song.all.count
+end
+
+
+
+
+end
